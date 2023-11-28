9296d95d5592d00fb7b2ec4a97af745e6bdf824c
Ayelen Valdez <ayelenvaldez07@gmail.com>
2023-11-28 17:48:55 -0300

    Add project files.

---
+++ ContosoUniversity/Models/Enrollment.cs
@@ -0,0 +1,22 @@
+﻿namespace ContosoUniversity.Models
+{
+    public class Enrollment
+    {
+        public int EnrollmentID { get; set; }
+        public int CourseID { get; set; }
+        public int StudentID { get; set; }
+
+        public Grade? Grade { get; set; }
+        public Student Student { get; set; }
+        public Course Course { get; set; }
+
+    }
+
+    public enum Grade
+    {
+        A = 'A',
+        B = 'B',
+        C = 'C',
+        F = 'F'
+    }
+}
